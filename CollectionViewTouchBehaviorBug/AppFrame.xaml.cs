abf704bbb038e7aed33a5f04a0993e934fb03f5d
Brandon Butler <brandon.butler@dornerworks.com>
2024-04-23 17:09:15 -0400

    add missing files

---
+++ CollectionViewTouchBehaviorBug/AppFrame.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionViewTouchBehaviorBug;
+
+public partial class AppFrame : ContentView
+{
+    public AppFrame()
+    {
+        InitializeComponent();
+    }
+}
